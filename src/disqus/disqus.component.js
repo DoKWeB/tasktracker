817b8de31043571d33a262cc007d2eb80fcd1f13
Aleksandr Ledyaev(EPAM) <Aleksandr_Lediaev@epam.com>
2018-03-22 16:59:47 +0400

    fix disqus component

--- src/disqus/disqus.component.js
+++ src/disqus/disqus.component.js
@@ -1,25 +1,31 @@
 class controller {
 	
     constructor($window, $location){
-		const url = $location.absUrl();
-		
-		if (!$window.DISQUS) {
-			var dsq = document.createElement('script');
-			dsq.type = 'text/javascript';
-			dsq.async = true;
-			dsq.src = '//' + this.config.disqus_shortname + '.disqus.com/embed.js';
-			(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-		} else {
-			$window.DISQUS.reset({
-				reload: true,
-				config: function () {
-					this.page.identifier = url + '/angular-esy-es';
-					this.page.url = url;
-					this.page.title = this.config.disqus_title;
-				}
-			});
-		}
-  }
+		this._$window = $window;
+		this._$location = $location;
+    }
+    
+    $onInit() {
+	    const url = this._$location.absUrl();
+	
+	    if (!this._$window.DISQUS) {
+		    var dsq = document.createElement('script');
+		    dsq.type = 'text/javascript';
+		    dsq.async = true;
+		    dsq.src = '//' + this.config.disqus_shortname + '.disqus.com/embed.js';
+		    dsq.setAttribute('data-timestamp', +new Date());
+		    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+	    } else {
+		    this._$window.DISQUS.reset({
+			    reload: true,
+			    config: function () {
+				    this.page.identifier = url + '/0e1fn0plhsul-000webhostapp-com';
+				    this.page.url = url;
+				    this.page.title = this.config.disqus_title;
+			    }
+		    });
+	    }
+    }
 }
 controller.$inject = ['$window', '$location'];
 
